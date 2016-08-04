98b3458d0851751758ef4843f593ec9963fc488f
黄晟楠 <hsnan@xiangyu.cn>
2016-08-04 20:58:30 +0800

    调整了nuget路径

--- nuget/package.bat
+++ nuget/package.bat
@@ -1,4 +1,4 @@
-nuget pack ..\Dian.NlogEx\Dian.NLogEx.csproj
+nuget pack ..\Dian.NlogEx\Dian.NLogEx.csproj -Build -Symbols -Properties Configuration=Release
 ping /n 2 127.1 >nul
 nuget push Dian.NLogEx*.nupkg e69a7d51-8f6f-495f-82ec-50d0db75cf42 -Source https://www.nuget.org/
 ping /n 2 127.1 >nul
