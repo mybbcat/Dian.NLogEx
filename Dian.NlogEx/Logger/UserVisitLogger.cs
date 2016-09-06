63f42d5c3680674cefdffe89a19dd196e1d3d7c7
黄晟楠 <hsnan@xiangyu.cn>
2016-09-06 17:55:50 +0800

    简化UserVisitLogger，调整输出的字段

--- Dian.NlogEx/Logger/UserVisitLogger.cs
+++ Dian.NlogEx/Logger/UserVisitLogger.cs
@@ -28,7 +28,7 @@ namespace Dian.NLogEx
                 { LogLevelEx.Error, NLog.LogLevel.Error },
                 { LogLevelEx.Fatal, NLog.LogLevel.Fatal }
             });
-        
+
         /// <summary>
         /// 输出带有客户端请求信息的日志
         /// </summary>
@@ -36,62 +36,24 @@ namespace Dian.NLogEx
         /// <param name="requestUrl">客户端请求的 URL</param>
         /// <param name="requestParameter">客户端请求的参数</param>
         /// <param name="requestBody">客户端请求的 Body</param>
-        /// <param name="logLevel">自定义日志级别 <see cref="LogLevel"/></param>
+        /// <param name="duration">执行请求至返回的总耗时（毫秒）</param>
+        /// <param name="statusCode">返回的HTTP状态码</param>
+        /// <param name="platform">模块或平台名称</param>
         /// <param name="message">自定义日志内容,支持格式化</param>
         /// <param name="args">与 message 结合使用，格式化的参数</param>
-        public static void WriteEx(string clientIp, string requestUrl, string requestParameter,
-            string requestBody, LogLevelEx logLevel, string message, params object[] args)
-        {
-            var logInfo = CreateLogEventInfo(clientIp, requestUrl, requestParameter, requestBody, logLevel);
-            logInfo.Message = string.Format(message, args);
-
-            _logger.Log(logInfo);
-        }
-
-        /// <summary>
-        /// 输出带有客户端请求信息的日志
-        /// </summary>
-        /// <param name="clientIp">客户端 IP</param>
-        /// <param name="requestUrl">客户端请求的 URL</param>
-        /// <param name="requestParameter">客户端请求的参数</param>
-        /// <param name="requestBody">客户端请求的 Body</param>
-        /// <param name="logLevel">自定义日志级别 <see cref="LogLevel"/></param>
-        /// <param name="message">自定义日志内容,支持格式化</param>
-        /// <param name="args">与 message 结合使用，格式化的参数</param>
-        /// <param name="ex">异常</param>
-        public static void WriteEx(string clientIp, string requestUrl, string requestParameter,
-                            string requestBody, LogLevelEx logLevel, Exception ex, string message, params object[] args)
-        {
-            LogEventInfo logInfo = CreateLogEventInfo(clientIp, requestUrl, requestParameter, requestBody, logLevel);
-            logInfo.Message = string.Format(message, args);
-            logInfo.Exception = ex;
-
-            _logger.Log(logInfo);
-        }
-
-        /// <summary>
-        /// 输出带有客户端请求信息的日志
-        /// </summary>
-        /// <param name="clientIp">客户端 IP</param>
-        /// <param name="requestUrl">客户端请求的 URL</param>
-        /// <param name="requestParameter">客户端请求的参数</param>
-        /// <param name="requestBody">客户端请求的 Body</param>
-        /// <param name="logLevel">自定义日志级别 <see cref="LogLevel"/></param>
-        /// <param name="ex">异常</param>
-        public static void WriteEx(string clientIp, string requestUrl, string requestParameter,
-                            string requestBody, LogLevelEx logLevel, Exception ex)
-        {
-            LogEventInfo logInfo = CreateLogEventInfo(clientIp, requestUrl, requestParameter, requestBody, logLevel);
-            logInfo.Exception = ex;
-
-            _logger.Log(logInfo);
-        }
-
-        private static LogEventInfo CreateLogEventInfo(string clientIp, string requestUrl, string requestParameter, string requestBody, LogLevelEx logLevel)
+        public static void Output(string clientIp, 
+                                    string requestUrl, 
+                                    string requestParameter,
+                                    string requestBody, 
+                                    long duration,
+                                    int statusCode,
+                                    string platform,
+                                    string message = null, 
+                                    params object[] args)
         {
             var logInfo = new LogEventInfo
             {
-                Level = LogLevelMap.Value[logLevel],
+                Level = NLog.LogLevel.Info,
                 LoggerName = CsLoggerName
             };
 
@@ -99,8 +61,15 @@ namespace Dian.NLogEx
             logInfo.Properties["requestUrl"] = requestUrl;
             logInfo.Properties["requestParameter"] = requestParameter;
             logInfo.Properties["requestBody"] = requestBody;
+            logInfo.Properties["duration"] = duration;
+            logInfo.Properties["statusCode"] = statusCode;
+            logInfo.Properties["platform"] = platform;
 
-            return logInfo;
+            if(!string.IsNullOrEmpty(message))
+                logInfo.Message = string.Format(message, args);
+
+            _logger.Log(logInfo);
         }
+
     }
 }
--- UnitTest/UnitTestProject1/UnitTest1.cs
+++ UnitTest/UnitTestProject1/UnitTest1.cs
@@ -3,6 +3,7 @@ using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Threading;
 using Dian.NLogEx;
+using Newtonsoft.Json.Bson;
 using NUnit.Framework;
 
 
@@ -15,7 +16,7 @@ namespace UnitTestProject1
         public void TestMethod1()
         {
             Logger.Fatal(GetType(), "This is a {0} log text!", new Exception(),"fatal");
-            Logger.Fatal(GetType(), "This is a  log text!");
+            Logger.Trace(GetType(), "This is a  log text!");
             Assert.IsTrue(true);
         }
 
@@ -30,6 +31,14 @@ namespace UnitTestProject1
                 Thread.Sleep(100);
             }
             Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void TestWebVisitToELK()
+        {
+//            UserVisitLogger.Output("127.0.0.1","Url","Parameter","Body",100,200,"测试平台");
+            UserVisitLogger.Output("127.0.0.1", "Url", "Parameter", "Body", 100, 200, "测试平台","message");
+            UserVisitLogger.Output("127.0.0.1", "Url", "Parameter", "Body", 100, 200, "测试平台", "message-{0}","you");
         }
     }
 }
